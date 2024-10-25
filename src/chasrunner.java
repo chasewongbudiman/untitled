d381ca4f7fedeec86942e2da0b451b5c464d14ac
BT_1N3_16 <sample@bths.edu>
2024-10-25 10:03:56 -0400

    commit commit

--- src/chasrunner.java
+++ src/chasrunner.java
@@ -1,5 +1,8 @@
 public class chasrunner {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("hihi");
+        String fart = "fart";
+        System.out.println(fart);
     }
 }
